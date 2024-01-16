bc8aaee2c43889a7de2c0b2b4fc32d9d7b68b00b
cblike <57317184+cblike@users.noreply.github.com>
2024-01-16 09:31:09 -0600

    Add files via upload

---
+++ gh_desktop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+https://github.com/shiftkey/desktop/releases/GitHubDesktop-linux-arm64-3.3.6-linux3.deb
+&& cd Downloads/
+&& sudo dpkg -i GitHubDesktop-linux-arm64-3.3.6-linux3.deb
+
